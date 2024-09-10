bf24de4883f9625aacc9f934dd85e23a3be59c8d
drTakuOmics <121636288+drTakuOmics@users.noreply.github.com>
2024-09-10 16:03:32 -0400

    HbYX-Second-Script.bash

--- HbYX-Second-Script.bash
+++ HbYX-Second-Script.bash
@@ -1,2 +1,14 @@
-#!bin/bash/ 
-awk ' BEGIN {OFS = "\n"} {header = $0 ; getline seq ; if (seq ~ /[G|L|I|V|A|Y|F|W|C|M ]Y[A-Z]$/){ taa=substr(seq,length(seq)-2,length(seq)) ;  print header"\n"taa}}'  multifasta.ol.fa  > 2aa.txt
+#!/bin/bash
+
+awk ' BEGIN {OFS = "\n"} {header = $0 ; getline seq ; if (seq ~ /[G|L|I|V|A|Y|F|W|C|M ]Y[A-Z]$/){ taa=substr(seq,length(seq)-2,length(seq)) ;  print header"\n"taa}}' multifasta.ol.fa > 2aa.txt
+# Explanation:
+# 'BEGIN {OFS = "\n"}': Set output field separator to newline
+# '{header = $0 ; getline seq': Store current line as header, read next line as sequence
+# 'if (seq ~ /[G|L|I|V|A|Y|F|W|C|M ]Y[A-Z]$/)': Check if sequence ends with HbYX-motif
+# 'taa=substr(seq,length(seq)-2,length(seq))': Extract last 3 amino acids
+# 'print header"\n"taa': Print header and extracted motif
+# 'multifasta.ol.fa': Input file (replace with your FASTA file if already in one-line-per-sequence format)
+# '> 2aa.txt': Output results to this file
+
+# Note: If your FASTA file is already in one-line-per-sequence format,
+# simply replace 'multifasta.ol.fa' with the name of your FASTA file and execute this script.
