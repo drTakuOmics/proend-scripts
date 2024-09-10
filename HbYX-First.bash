6735a623ab94b1cf12b6d7165dfa908656ec9f34
drTakuOmics <121636288+drTakuOmics@users.noreply.github.com>
2024-09-10 16:01:39 -0400

    Update HbYX-First.bash

--- HbYX-First.bash
+++ HbYX-First.bash
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-# This script processes a FASTA file to reformat it into a single line per sequence format
-
-# Use awk to process the input file
-awk '
-    /^>/ {
-        # If the line starts with ">", it's a header
-        # Print a newline (except for the first header), then print the header
-        printf("\n%s\n", $0);
-        next;  # Skip to the next line
-    }
-    {
-        # For sequence lines, print without newline
-        printf("%s", $0);
-    }
-    END {
-        # After processing all lines, print a final newline
-        printf("\n");
-    }
-' UP000002199_224325.fasta |  # Input file name
-    grep -v ^$ > multifasta.ol.fa  # Remove empty lines and save to output file
-
-# Explanation of the grep command:
-# -v: Invert the match, i.e., select non-matching lines
-# ^$: Regex pattern for an empty line (start of line immediately followed by end of line)
-# > multifasta.ol.fa: Redirect output to this file
+awk '/^>/{printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' UP000002199_224325.fasta | grep -v ^$ > multifasta.ol.fa
+# Explanation:
+# awk '/^>/ {...}': Process the input FASTA file
+#   '/^>/': If line starts with ">", it's a header
+#   'printf("\n%s\n",$0)': Print newline, then header, then another newline
+#   'next': Skip to next line
+#   '{ printf("%s",$0);}': For sequence lines, print without newline
+#   'END {printf("\n");}': Print final newline after processing all lines
+# 'UP000002199_224325.fasta': Input file name
+# '| grep -v ^$': Pipe output to grep, removing empty lines
+# '> multifasta.ol.fa': Redirect final output to this file
